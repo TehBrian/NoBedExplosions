a2cb3510103efe894572bdfc182e045212ba588e
TehBrian <tehbrian@outlook.com>
2020-09-08 13:41:51 -0400

    Add multi-world support and redo config

--- src/main/java/xyz/tehbrian/nobedexplosions/listeners/BedListener.java
+++ src/main/java/xyz/tehbrian/nobedexplosions/listeners/BedListener.java
@@ -1,6 +1,8 @@
 package xyz.tehbrian.nobedexplosions.listeners;
 
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -8,6 +10,7 @@ import org.bukkit.event.player.PlayerBedEnterEvent;
 import xyz.tehbrian.nobedexplosions.NoBedExplosions;
 import xyz.tehbrian.nobedexplosions.util.MessageUtils;
 
+import java.util.Locale;
 import java.util.Objects;
 
 @SuppressWarnings("unused")
@@ -22,19 +25,36 @@ public class BedListener implements Listener {
     @EventHandler
     public void onBedEnter(PlayerBedEnterEvent event) {
         FileConfiguration config = main.getConfig();
-
         if (!config.getBoolean("enabled")) return;
-        if (event.getBedEnterResult() != PlayerBedEnterEvent.BedEnterResult.NOT_POSSIBLE_HERE) return;
 
-        if (config.getBoolean("allow_sleep")) {
-            event.setUseBed(Event.Result.ALLOW);
-        } else {
-            event.setCancelled(true);
+        Player player = event.getPlayer();
+        ConfigurationSection worldSettings = config.getConfigurationSection("worlds." + player.getWorld().getName());
+        if (worldSettings == null) return;
 
-            String deniedMsg = main.getMessage("denied_msg");
-            if (!deniedMsg.isEmpty()) {
-                event.getPlayer().sendMessage(deniedMsg);
-            }
+        Mode mode = Mode.valueOf(Objects.requireNonNull(worldSettings.getString("mode")).trim().toUpperCase(Locale.ENGLISH));
+        switch (mode) {
+            case ALLOW:
+                if (event.getBedEnterResult() == PlayerBedEnterEvent.BedEnterResult.NOT_POSSIBLE_HERE) {
+                    event.setUseBed(Event.Result.ALLOW);
+                }
+                break;
+            case DENY:
+                event.setUseBed(Event.Result.DENY);
+
+                String denyMsg = MessageUtils.color(worldSettings.getString("deny_msg"));
+                if (denyMsg != null && !denyMsg.isEmpty()) {
+                    event.getPlayer().sendMessage(denyMsg);
+                }
+                break;
+            case DEFAULT:
+                event.setUseBed(Event.Result.DEFAULT);
+                break;
         }
+    }
+
+    private enum Mode {
+        ALLOW,
+        DENY,
+        DEFAULT,
     }
 }
