93b093c2f221bcb7a424358078a283abb723ecd3
TehBrian <tehbrian@protonmail.com>
2022-01-14 12:38:33 -0500

    build: use Java toolchain

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,7 @@ group = "xyz.tehbrian"
 version = "2.0.1"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
 }
 
 repositories {
