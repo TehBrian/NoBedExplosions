b32facd40d884cc2f6af33ab64b21cc7edce2c61
TehBrian <tehbrian@proton.me>
2023-10-21 14:26:19 -0400

    update deps; upgrade to Gradle 8.4; mc 1.20.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
   id("java")
   id("com.github.johnrengelman.shadow") version "8.1.1"
-  id("xyz.jpenilla.run-paper") version "2.1.0"
+  id("xyz.jpenilla.run-paper") version "2.2.0"
   id("net.kyori.indra.checkstyle") version "3.1.3"
-  id("com.github.ben-manes.versions") version "0.47.0"
+  id("com.github.ben-manes.versions") version "0.49.0"
 }
 
 group = "dev.tehbrian"
@@ -22,11 +22,11 @@ repositories {
 }
 
 dependencies {
-  compileOnly("io.papermc.paper:paper-api:1.20.1-R0.1-SNAPSHOT")
+  compileOnly("io.papermc.paper:paper-api:1.20.2-R0.1-SNAPSHOT")
 
   implementation("com.google.inject:guice:7.0.0")
   implementation("dev.tehbrian:tehlib-paper:0.5.0")
-  implementation("cloud.commandframework:cloud-paper:1.8.3")
+  implementation("cloud.commandframework:cloud-paper:1.8.4")
   implementation("org.spongepowered:configurate-yaml:4.1.2")
 }
 
@@ -68,6 +68,6 @@ tasks {
   }
 
   runServer {
-    minecraftVersion("1.20.1")
+    minecraftVersion("1.20.2")
   }
 }
