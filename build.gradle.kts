eead398f6a49336b522c31fa6e95b22931417b8c
TehBrian <tehbrian@proton.me>
2026-05-28 19:10:40 -0400

    version 2.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "dev.tehbrian"
-version = "2.2.8-SNAPSHOT"
+version = "2.3.0"
 description = "Control bed and respawn anchor functionality across all of" +
 		" your worlds, such as allowing sleep in the nether or the end!"
 
